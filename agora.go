e040f7fc168bcb6c7ec919b8cc9b5fa14dcd2d46
wgentry22 <william.gentry02@gmail.com>
2021-01-31 13:15:22 -0500

    Implemented interfaces for `broker.Consumer`, `broker.Publisher`, and `broker.Event`
    - Also properly configure publisher or subscriber based on config file
    - Streamline errors via the `Errors() <-chan error` implementations

--- agora.go
+++ agora.go
@@ -1,107 +1,140 @@
 package agora
 
 import (
-	"context"
-	"errors"
-	"net/http"
-	"os"
-	"os/signal"
-	"syscall"
-	"time"
+  "context"
+  "errors"
+  "net/http"
+  "os"
+  "os/signal"
+  "syscall"
+  "time"
 
-	"github.com/hashicorp/errwrap"
-	"github.com/wgentry22/agora/modules/api"
-	"github.com/wgentry22/agora/modules/heartbeat"
-	"github.com/wgentry22/agora/modules/logg"
-	"github.com/wgentry22/agora/modules/orm"
-	"github.com/wgentry22/agora/types/config"
+  "github.com/hashicorp/errwrap"
+  "github.com/wgentry22/agora/modules/api"
+  "github.com/wgentry22/agora/modules/broker"
+  "github.com/wgentry22/agora/modules/heartbeat"
+  "github.com/wgentry22/agora/modules/logg"
+  "github.com/wgentry22/agora/modules/orm"
+  "github.com/wgentry22/agora/types/config"
 )
 
 var (
-	logger logg.Logger
+  logger    logg.Logger
+  consumer  broker.Consumer
+  publisher broker.Publisher
 )
 
 type Application struct {
-	errors chan error
-	conf   config.Application
-	router api.Router
-	quit   chan os.Signal
+  errors chan error
+  conf   config.Application
+  router api.Router
+  quit   chan os.Signal
 }
 
 func (a *Application) RegisterController(controllers ...api.Controller) {
-	for _, controller := range controllers {
-		a.router.Register(controller)
-	}
+  for _, controller := range controllers {
+    a.router.Register(controller)
+  }
 }
 
 func (a *Application) Errors() <-chan error {
-	return a.errors
+  return a.errors
 }
 
 func (a *Application) Logger() logg.Logger {
-	return logg.NewLogrusLogger(a.conf.Logging())
+  return logg.NewLogrusLogger(a.conf.Logging())
 }
 
 func (a *Application) Router() http.Handler {
-	return a.router.Handler()
+  return a.router.Handler()
+}
+
+func (a *Application) Publisher() broker.Publisher {
+  return publisher
+}
+
+func (a *Application) Consumer() broker.Consumer {
+  return consumer
 }
 
 func (a *Application) Setup() {
-	go func() {
-		if err, ok := <-a.errors; ok {
-			logger.WithError(err).Panic("Unable to continue")
-		}
-	}()
+  go func() {
+    if err, ok := <-a.errors; ok {
+      logger.WithError(err).Panic("Unable to continue")
+    }
+  }()
 
-	defer func() {
-		if r := recover(); r != nil {
-			err, isErr := r.(error)
-			if isErr {
-				errwrap.Walk(err, func(e error) {
-					logger.WithError(e).Error("Found wrapped error")
-				})
-				a.errors <- err
-			}
-		}
-	}()
+  defer func() {
+    if r := recover(); r != nil {
+      err, isErr := r.(error)
+      if isErr {
+        errwrap.Walk(err, func(e error) {
+          logger.WithError(e).Error("Found wrapped error")
+        })
+        a.errors <- err
+      }
+    }
+  }()
 
-	logger = logg.NewLogrusLogger(a.conf.Logging())
+  logger = logg.NewLogrusLogger(a.conf.Logging())
 
-	orm.UseConfig(a.conf.DB())
-	orm.UseLoggingConfig(a.conf.Logging())
-	orm.RegisterPulser()
-	orm.RegisterPacer()
+  orm.UseConfig(a.conf.DB())
+  orm.UseLoggingConfig(a.conf.Logging())
+  orm.RegisterPulser()
+  orm.RegisterPacer()
 
-	a.router.Register(heartbeat.NewHeartbeatController(a.conf.Heartbeat()))
+  a.router.Register(heartbeat.NewHeartbeatController(a.conf.Heartbeat()))
+
+  if a.conf.Broker().Role == config.RoleProducer {
+    publisher = broker.NewPublisher(a.conf.Broker())
+  } else if a.conf.Broker().Role == config.RoleConsumer {
+    consumer = broker.NewConsumer(a.conf.Broker())
+  }
 }
 
 func (a *Application) Start() {
-	server := a.router.Server()
+  server := a.router.Server()
 
-	signal.Notify(a.quit, syscall.SIGINT, syscall.SIGTERM)
+  signal.Notify(a.quit, syscall.SIGINT, syscall.SIGTERM)
 
-	go func() {
-		if err, ok := <-a.errors; ok {
-			logger.WithError(err).Error("Error while using application")
-			a.quit <- syscall.SIGTERM
-		}
-	}()
+  go func() {
+    if err, ok := <-a.errors; ok {
+      logger.WithError(err).Error("Error while using application")
+      a.quit <- syscall.SIGTERM
+    }
+  }()
 
-	go func() {
-		if err := server.ListenAndServe(); err != nil && !errors.Is(err, http.ErrServerClosed) {
-			a.errors <- err
-		}
-	}()
+  go func() {
+    if err := server.ListenAndServe(); err != nil && !errors.Is(err, http.ErrServerClosed) {
+      a.errors <- err
+    }
+  }()
 
-	<-a.quit
-	logger.Info("Shutting down server...")
+  if consumer != nil {
+    go func(c broker.Consumer) {
+      if err := <-c.Errors(); err != nil {
+        a.errors <- err
+      }
+    }(consumer)
+  }
 
-	withTimeout, cancel := context.WithTimeout(context.Background(), 5*time.Second)
-	defer cancel()
+  if publisher != nil {
+    go func(p broker.Publisher) {
+      if err := <-p.Errors(); err != nil {
+        a.errors <- err
+      }
+    }(publisher)
+  }
 
-	if err := server.Shutdown(withTimeout); err != nil {
-		logger.WithError(err).Panic("Failed to shutdown server")
-	}
+  <-a.quit
+  logger.Info("Shutting down server...")
 
-	logger.Info("Server exiting.")
+  withTimeout, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+  defer cancel()
+
+  if err := server.Shutdown(withTimeout); err != nil {
+    logger.WithError(err).Panic("Failed to shutdown server")
+  }
+
+  logger.Info("Server exiting.")
 }
--- types/config/broker.go
+++ types/config/broker.go
@@ -9,7 +9,21 @@ import (
   "gopkg.in/confluentinc/confluent-kafka-go.v1/kafka"
 )
 
+type BrokerRole int8
+
+const (
+  RoleUnknown BrokerRole = iota
+  RoleProducer
+  RoleConsumer
+)
+
 var (
+  brDisplay = []string{"unknown", "producer", "consumer"}
+  brLookup  = map[string]BrokerRole{
+    "unknown":  RoleUnknown,
+    "producer": RoleProducer,
+    "consumer": RoleConsumer,
+  }
   ErrBrokerIDRequired        = errors.New("value for `broker.id` is expected")
   ErrBrokerServersRequired   = errors.New("values for `broker.servers` must be present")
   ErrFailedToSetBrokerConfig = func(key string, value interface{}) error {
@@ -17,10 +31,17 @@ var (
   }
 )
 
+func (br BrokerRole) String() string {
+  return brDisplay[br]
+}
+
 type Broker struct {
-  ID      string                 `toml:"id"`
-  Servers []string               `toml:"servers"`
-  Args    map[string]interface{} `toml:"args"`
+  ID         string                 `toml:"id"`
+  Role       BrokerRole             `toml:"role"`
+  Servers    []string               `toml:"servers"`
+  BufferSize int                    `toml:"buffer_size"`
+  Timeout    int                    `toml:"timeout"`
+  Args       map[string]interface{} `toml:"args"`
 }
 
 func (b *Broker) UnmarshalTOML(data interface{}) (err error) {
@@ -30,6 +51,13 @@ func (b *Broker) UnmarshalTOML(data interface{}) (err error) {
     b.ID = id
   } else {
     err = errwrap.Wrap(ErrBrokerIDRequired, err)
+  }
+
+  if role, ok := dataMap["role"].(string); ok {
+    found, roleFound := brLookup[role]
+    if roleFound {
+      b.Role = found
+    }
   }
 
   if vals, ok := dataMap["servers"].([]interface{}); ok {
@@ -45,6 +73,18 @@ func (b *Broker) UnmarshalTOML(data interface{}) (err error) {
 
   if args, ok := dataMap["args"].(map[string]interface{}); ok {
     b.Args = args
+  }
+
+  if buffer, ok := dataMap["buffer_size"].(int64); ok {
+    b.BufferSize = int(buffer)
+  } else {
+    b.BufferSize = 100
+  }
+
+  if timeout, ok := dataMap["timeout"].(int64); ok {
+    b.Timeout = int(timeout)
+  } else {
+    b.Timeout = 150
   }
 
   return err
